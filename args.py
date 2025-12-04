f738537fec80b7351cd885665fdcc0d7b5572d59
Nikolay Krasko <nikolay.krasko@jetbrains.com>
2025-12-05 00:02:54 +0100

    Move credentials to the secret folder

--- args.py
+++ args.py
@@ -1,8 +1,8 @@
-SOURCE_OAUTH_JSON = "credentials_source.json"
-SOURCE_TOKEN_FILE = "token_source.json"
+SOURCE_OAUTH_JSON = "secret/credentials_source.json"
+SOURCE_TOKEN_FILE = "secret/token_source.json"
 
-TARGET_OAUTH_JSON = "credentials_target.json"
-TARGET_TOKEN_FILE = "token_target.json"
+TARGET_OAUTH_JSON = "secret/credentials_target.json"
+TARGET_TOKEN_FILE = "secret/token_target.json"
 
 SOURCE_CALENDAR_ID = "primary"
 TARGET_CALENDAR_ID = "primary"
@@ -10,4 +10,4 @@ TARGET_CALENDAR_ID = "primary"
 SOURCE_SCOPES = ["https://www.googleapis.com/auth/calendar.readonly"]
 TARGET_SCOPES = ["https://www.googleapis.com/auth/calendar.events"]
 
-HOURS_AHEAD = 24
+HOURS_AHEAD = 48
