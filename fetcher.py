3be5f2aefc18dce079fcf1a2b11908d25abb8adc
Tanya Abramovskaya <tanya.abramovskaya@gmail.com>
2025-12-05 21:21:35 +0100

    Changing the time interval for the update from "from now" to "from today’s midnight"

--- fetcher.py
+++ fetcher.py
@@ -15,10 +15,13 @@ def is_cancelled(ev):
 
 def fetch_upcoming_events(service, calendar_id: str, hours: int):
     tzinfo = tz.gettz("UTC")
-    now = dt.datetime.now(tzinfo)
-    end = now + dt.timedelta(hours=hours)
+    today = dt.datetime.now(tzinfo).date()
+    start = dt.datetime.combine(today, dt.time.min, tzinfo)
 
-    time_min = now.isoformat()
+    # N hours from midnight
+    end = start + dt.timedelta(hours=hours)
+
+    time_min = start.isoformat()
     time_max = end.isoformat()
 
     resp = service.events().list(
@@ -31,14 +34,15 @@ def fetch_upcoming_events(service, calendar_id: str, hours: int):
     ).execute()
 
     events = resp.get("items", [])
-    cleaned = []
 
+    # Filter out all-day events and cancelled events
+    filtered = []
     for ev in events:
         if is_cancelled(ev):
             continue
         if is_all_day(ev):
             continue
-        cleaned.append(ev)
+        filtered.append(ev)
 
-    log.info(f"Found {len(cleaned)} events to copy.")
-    return cleaned
+    log.info(f"Found {len(filtered)} events to copy.")
+    return filtered
